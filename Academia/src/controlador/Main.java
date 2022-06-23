18b9eea91ba94b33bce2a4acc76897f3a9094489
AlejandroMoncada <alejandrolopezmoncada10@gmail.com>
2022-06-22 19:16:36 -0500

    Se termino registro

--- Academia/src/controlador/Main.java
+++ Academia/src/controlador/Main.java
@@ -1,5 +1,6 @@
 package controlador;
 
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -7,7 +8,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+
 	@Override
 	public void start(Stage stage) {
 		try {
--- Academia/src/controlador/registerController.java
+++ Academia/src/controlador/registerController.java
@@ -4,15 +4,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 import modelo.Conexion;
 
 public class registerController {
@@ -47,15 +43,15 @@ public class registerController {
 	
 	@FXML
 	void registrarse(MouseEvent event) throws SQLException{
+		
 			String inpId = inputId.getText();
 			String inpName = inputName.getText();
 			String inpLastName = inputLastName.getText();
 			String inpEmail = inputEmail.getText();
 			String inpPassword = inputPassword.getText();
 			String inpAge = inputAge.getText();
-			//String cmbType = cmbSelector.getValue();
-			
-			
+			String cmbType = cmbSelector.getValue();
+	
 			if(inpId==null || inpId.isEmpty())
 		        txtEntrada.setText("Debe ingresar una Identificacion valida");
 		    else if(inpName==null || inpName.isEmpty())
@@ -65,44 +61,40 @@ public class registerController {
 		    else if(inpEmail==null || inpEmail.isEmpty())
 		        txtEntrada.setText("Debe ingresar un Email valido");
 		    else if(inpPassword==null || inpPassword.isEmpty())
-		        txtEntrada.setText("Debe ingresar un Contraseña valida");
+		        txtEntrada.setText("Debe ingresar un ContraseÃ±a valida");
 		    else if(inpAge==null || inpAge.isEmpty())
 		        txtEntrada.setText("Debe ingresar una Edad valida");
-		    //else if(cmbType==null || cmbType.isEmpty())
-		        //txtEntrada.setText("Debe ingresar un tipo de persona valido");
-		    else{
-				String query = "INSERT into personas (id,nombre,apellido,email,edad,pw) values ('"+inpId+"','"+inpName+"','"+inpLastName+"','"+inpEmail+"','"+inpAge+"','"+Integer.parseInt(inpPassword)+"')";
-				con.conectar();
-				
-				 try (Statement stm = con.getCon().createStatement()){
-			            int rest = stm.executeUpdate(query);
-			            if(rest != 0){
-			                txtEntrada.setText("Datos Registrados con exito");
-			                restaurarDatos();
-			                
-			                Stage stage = (Stage) btnRegister.getScene().getWindow();
-		                    stage.close();
-			                
-			                FXMLLoader loader = new FXMLLoader(getClass().getResource("../vista/vistaPrincipal.fxml"));
-		                    Parent root = loader.load();
-		                    Scene scene = new Scene(root);
-		                    stage = new Stage();
-		                   
-		                    stage.setTitle("Iniciar Sesion");
-		                    stage.setScene(scene);
-		                  
-		                    stage.show();
-		                    con.desconectar();
-			            }
-			            else{
-			                txtEntrada.setText("Error al grabar los datos por favor verifique");
-			            }
-			        } catch (Exception e) {
-			            e.printStackTrace();
-			        }
-			        con.desconectar();
-			}
-		}
+		    else if(cmbType==null || cmbType.isEmpty())
+		        txtEntrada.setText("Debe ingresar un tipo valido");
+		    else {
+		    	if(cmbType == "Instructor"){
+					String query = "INSERT into personas (id,nombre,apellido,email,edad,pw,tipo) values ('"+inpId+"','"+inpName+"','"+inpLastName+"','"+inpEmail+"','"+inpAge+"','"+inpPassword+"','"+1+"')";
+					con.conectar();
+					Answer(query);
+		    	}else if(cmbType == "Aprendiz"){
+		    		String query = "INSERT into personas (id,nombre,apellido,email,edad,pw,tipo) values ('"+inpId+"','"+inpName+"','"+inpLastName+"','"+inpEmail+"','"+inpAge+"','"+inpPassword+"','"+0+"')";
+					con.conectar();
+					Answer(query);
+		    	}
+		    }
+		}	
+	
+		
+	private void Answer(String query){
+		 try (Statement stm = con.getCon().createStatement()){
+	            int rest = stm.executeUpdate(query);
+	            if(rest != 0){
+	                txtEntrada.setText("Datos Registrados con exito");
+	                restaurarDatos();
+	            }
+	            else{
+	                txtEntrada.setText("Error al grabar los datos por favor verifique");
+	            }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        con.desconectar();
+		} 
 	
 	private void restaurarDatos(){
 	    inputName.clear();
@@ -113,9 +105,9 @@ public class registerController {
 	}
 	
 	@FXML
-	void cmbRegister(){ 
+	void initialize(){ 
 		cmbSelector.getItems().clear();
 		cmbSelector.getItems().addAll("Instructor", "Aprendiz");
-	    cmbSelector.setValue("Tipo de persona");
+	  
 	}
 }
